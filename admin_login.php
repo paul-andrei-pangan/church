94fb2566a2d9b3563a9021c040f544482c05c3dc
draizen007 <pedokdelacruz123@gmail.com>
2025-02-05 02:15:22 +0800

    cdsa

--- admin_login.php
+++ admin_login.php
@@ -29,14 +29,21 @@ if (isset($_POST['admin_login'])) {
     <title>Admin Login</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
-    <style>
-        body {
-            background-image: url('img/admin.jpg'); 
-            background-size: cover;
-            background-position: center;
-            background-repeat: no-repeat;
-        }
-    </style>
+   <style>
+    body {
+        background-image: url('img/admin.jpg'); 
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+    }
+
+    .card {
+        background: rgba(255, 255, 255, 0.7);
+        backdrop-filter: blur(5px); 
+        border-radius: 10px;
+    }
+</style>
+
 </head>
 <body class="bg-light">
 
